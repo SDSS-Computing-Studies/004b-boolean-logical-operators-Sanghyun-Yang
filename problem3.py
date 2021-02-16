13f1e4b9379af808a9e3957a357ece50a74775a8
Sanghyun-Yang <78501215+Sanghyun-Yang@users.noreply.github.com>
2021-02-16 09:18:07 -0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -26,3 +26,93 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+x = int(input())
+y = int(input())
+z = int(input())
+
+if x > y and x > z:
+    longest = x
+    if y > z: 
+        middle = y
+        shortest = z
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " form a Pythagorean triple")
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " do not form a Pythagorean triple")
+    elif z > y: 
+        middle = z
+        shortest = y
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," +longest + " form a Pythagorean triple" )
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " do not form a Pythagorean triple")
+elif y > x and y > z:
+    longest = y
+    if x > z: 
+        middle = x
+        shortest = z
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " form a Pythagorean triple" )
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " do not form a Pythagorean triple")
+    elif z > x: 
+        middle = z
+        shortest = x
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " form a Pythagorean triple" )
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " do not form a Pythagorean triple")
+elif z > x and z > y:
+    longest = z
+    if y > x: 
+        middle = y
+        shortest = x
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," +longest + " form a Pythagorean triple" )
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," + longest + " do not form a Pythagorean triple")
+    elif x > y: 
+        middle = x
+        shortest = y 
+        if longest ** 2 == middle ** 2 + shortest ** 2:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," +longest + " form a Pythagorean triple" )
+        else:
+            longest = str(longest)
+            middle = str(middle)
+            shortest = str(shortest)
+            print(shortest + "," + middle + "," +longest + " do not form a Pythagorean triple")
+
+
